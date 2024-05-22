e2f380b2896211419836cb523281f7df7ff73278
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-22 15:55:59 +0200

    WIP golangci, commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,18 +1,42 @@
+/* eslint-disable unicorn/prefer-module */
+/* eslint-disable no-magic-numbers */
 // .commitlintrc.js
 const fs = require('node:fs');
 const path = require('node:path');
-
-// const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
-const apps = fs.readdirSync(path.resolve(__dirname, 'apps'));
-const libs = fs.readdirSync(path.resolve(__dirname, 'libs'));
-
 const { execSync } = require('child_process');
 
-// @tip: git branch name = feature/issue_33   =>    auto get defaultIssues = #33
+// can propose scope for commit using folder names
+const apps = fs.readdirSync(path.resolve(__dirname, 'src'));
+
+// can find issues from branch name if standardized
+// @tip: git branch name = feature/33-issuename   =>    auto get defaultIssues = #33
+// const issue = execSync("git rev-parse --abbrev-ref HEAD")
+//   .toString()
+//   .trim()
+//   .split("-")[0];
+
+// find the number in the string
 const issue = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim()
-  .split('_')[1];
+  .match(/\d+/)?.[0];
+
+// manually adding scopes examples
+// ...["app", "gradle", "npm", "git-hooks"],
+const definedScopes = [
+  // used to denote global changes
+  'global',
+  // spread result of folder names found
+  ...apps.map((app) => `app-${app}`),
+  // "app",
+  'gradle',
+  'npm',
+  'git-hooks',
+  'actions',
+  'github',
+  'db',
+  'docs',
+];
 
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
@@ -20,23 +44,33 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
-    'scope-enum': [
-      2,
-      'always',
-      [
-        // ...packages,
-        'global', // used to denote global changes
-        ...apps,
-        ...libs,
-      ],
-    ],
+    'scope-enum': [2, 'always', [...definedScopes]],
   },
   prompt: {
-    alias: { fd: 'docs: fix typos' },
     useEmoji: true,
-    useAI: false,
-    aiNumber: 1,
     customIssuePrefixAlign: !issue ? 'top' : 'bottom',
     defaultIssues: !issue ? '' : `#${issue}`,
+    issuePrefixes: [
+      {
+        name: 'Close issue',
+        value: 'closes',
+      },
+      {
+        name: 'Fix issue',
+        value: 'fixes',
+      },
+      {
+        name: 'Link issue',
+        value: 'links',
+      },
+      {
+        name: 'Reference issue',
+        value: 'refs',
+      },
+    ],
+
+    // allow defining multiple scopes with checklist
+    enableMultipleScopes: true,
+    scopeEnumSeparator: '/',
   },
 };
