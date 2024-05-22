a3f0976f06bd757f22565c203c5088aecb145439
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-22 21:11:51 +0200

    WIP go mono module

--- apps/its-battistar-be-go/cmd/server/main.go
+++ apps/its-battistar-be-go/cmd/server/main.go
@@ -1,31 +1,7 @@
-//nolint:godot // ignore godot for swagger
 package main
 
-import (
-	"fmt"
+import "log"
 
-	"github.com/gipo355/its-battistar-be-go/docs"
-	"github.com/gipo355/its-battistar-be-go/internal/config"
-	"github.com/gipo355/its-battistar-be-go/pkg/app"
-)
-
-//	@title			Echo Battistar API
-//	@version		1.0
-//	@description	Echo Battistar API
-
-//	@contact.name	gipo355
-//	@contact.url	github.com/gipo355
-//	@contact.email	github.com/gipo355
-
-//	@securityDefinitions.apikey	ApiKeyAuth
-//	@in							header
-//	@name						Authorization
-
-// @BasePath	/
 func main() {
-	cfg := config.New()
-
-	docs.SwaggerInfo.Host = fmt.Sprintf("%s:%s", cfg.HTTP.Host, cfg.HTTP.Port)
-
-	app.Start(cfg)
+	log.Println("Starting server...")
 }
