84280b51b5ac77383d4a9b9accf14b6bf7598f55
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-22 20:57:21 +0200

    WIP add go project

---
+++ apps/its-battistar-be-go/internal/repository/user_repository.go
@@ -0,0 +1,23 @@
+package repositories
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/gipo355/its-battistar-be-go/internal/models"
+)
+
+type IUserRepository interface {
+	GetUserByEmail(user *models.User, email string)
+}
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{DB: db}
+}
+
+func (userRepository *UserRepository) GetUserByEmail(user *models.User, email string) {
+	userRepository.DB.Where("email = ?", email).Find(user)
+}
