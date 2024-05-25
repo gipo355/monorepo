80781baf6ef46ff6e46645f907bdc2b50acca3bf
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-25 11:55:23 +0200

    WIP update say hello cross

---
+++ .changeset/twelve-squids-wait.md
@@ -0,0 +1,5 @@
+---
+'@gipo355/say-hello-cross': minor
+---
+
+add typescript info for testing release
--- libs/say-hello-cross/src/lib/say-hello-cross.ts
+++ libs/say-hello-cross/src/lib/say-hello-cross.ts
@@ -1,3 +1,24 @@
-export function sayHelloCross(): string {
-  return 'say-hello-cross';
+/**
+ * @description
+ * This is a cross platform function that returns a string equals to the input
+ */
+interface ISayHelloCrossOptions {
+  /**
+   * @description output string which will be returned
+   */
+  output: string;
+}
+
+/**
+ * Say hello cross
+ * @description it returns a string equals to the input
+ * @param {ISayHelloCrossOptions} opts
+ * @returns string
+ * @example
+ * ```ts
+ * const result = sayHelloCross();
+ * ```
+ */
+export function sayHelloCross(opts: ISayHelloCrossOptions): string {
+  return opts.output;
 }
