93a22a91da19ca14bfa6a10773e5f5bad435a69f
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-25 13:06:01 +0200

    WIP export interface

--- libs/say-hello-cross/src/lib/say-hello-cross.ts
+++ libs/say-hello-cross/src/lib/say-hello-cross.ts
@@ -1,19 +1,15 @@
 /**
- * @description
  * This is a cross platform function that returns a string equals to the input
  */
-interface ISayHelloCrossOptions {
+export interface ISayHelloCrossOptions {
   /**
-   * @description output string which will be returned
+   * output string which will be returned
    */
   output: string;
 }
 
 /**
- * Say hello cross
- * @description it returns a string equals to the input
- * @param {ISayHelloCrossOptions} opts
- * @returns string
+ * Say hello cross returns a string
  */
 export function sayHelloCross(opts: ISayHelloCrossOptions): string {
   return opts.output;
