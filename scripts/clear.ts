ef69d1cc5cf96ee2c730f504a0c96e2b1c28eb22
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-05-26 17:43:10 +0200

    WIP scripts

--- scripts/clear.ts
+++ scripts/clear.ts
@@ -8,9 +8,31 @@ const args = process.argv.slice(2);
  */
 const cmds = {
   folders:
-    'find . -type d \\( -name node_modules -o -name tsconfig.tsbuildinfo -o -name dist -o -name coverage -o -name .eslintcache -o -name .cache -o -name .stylelintcache -o -name test-dist \\) -exec rm -rf {} +',
+    `find . \
+-type d \
+( \
+-name .angular \
+-o -name .nx \
+-o -name node_modules \
+-o -name tsconfig.tsbuildinfo \
+-o -name dist \
+-o -name coverage \
+-o -name .eslintcache \
+-o -name .cache \
+-o -name .stylelintcache \
+-o -name test-dist \
+) \
+-exec rm -rf {} +`,
+
   cache:
-    'find . -type d \\( -name .eslintcache -o -name .cache -o -name .stylelintcache \\) -exec rm -rf {} +',
+    `find . \
+  -type d \
+( \
+-name .eslintcache \
+-o -name .cache \
+-o -name .stylelintcache \
+) \
+-exec rm -rf {} +`,
 };
 
 /**
