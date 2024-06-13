8bde5bb645ed2e07bfe1dee9134f24129cc8feec
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-13 22:21:04 +0200

    docs: add docs to nestutils response dto

--- packages/nest-utils/src/lib/dtos/response.dto.ts
+++ packages/nest-utils/src/lib/dtos/response.dto.ts
@@ -1,14 +1,34 @@
+import type { HttpStatus } from '@nestjs/common';
+import type { Static } from '@sinclair/typebox';
+
 /**
  * IBaseResponse is a generic interface that defines the structure of the response object.
  * Provides a common structure for all responses.
  * Being an interface, it can be extended with another interface simply by shadowing it.
  */
 export interface IBaseResponse<T> {
+  /**
+   * The data to be encapsulated in the response object.
+   */
   data?: T;
+  /**
+   * The length of the data array.
+   */
   length?: number;
+  /**
+   * Optional message to be sent in the response object.
+   */
   message?: string;
+  /**
+   * Utility key to determine if the request was successful.
+   */
   ok: boolean;
+  /**
+   * The status code of the response. Uses NestJS HttpStatus enum.
+   */
+  statusCode?: HttpStatus;
 }
+export type BaseResponseDto = Static<typeof BaseResponseDtoSchema>;
 
 /**
  * WithRequired is a utility type that makes the specified keys required for a given type or interface.
@@ -25,7 +45,14 @@ export type WithRequired<T, K extends keyof T> = Omit<T, K> &
  * @param L - The union type of the required keys that the response object can have. Default is standard IBaseResponse<T>.
  *
  * @example
- * TResponseDto<UserDto, 'message'> - Response object with a message key.
+ * TResponseDto<{user: UserDto}, 'message'> - Response object with a message key.
+ * Provides:
+ * {
+ *  ok: boolean,
+ *  length?: number,
+ *  message: string,
+ *  data: {user: UserDto},
+ * }
  */
 export type TResponseDto<
   T,
