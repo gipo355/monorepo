930bd0951b34bb4f0aea905e430cc577e2fd5496
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-06-13 20:52:11 +0200

    chore: prepare config files for nest-utils

--- packages/nest-utils/eslint.config.js
+++ packages/nest-utils/eslint.config.js
@@ -1,11 +1,42 @@
+const globals = require('globals');
 const baseConfig = require('../../eslint.config.js');
+const tseslint = require('typescript-eslint');
+// const { FlatCompat } = require('@eslint/eslintrc');
+// const js = require('@eslint/js');
 
-module.exports = [
+const pluginSecurity = require('eslint-plugin-security');
+const nodePlugin = require('eslint-plugin-n');
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   recommendedConfig: js.configs.recommended,
+// });
+
+module.exports = tseslint.config(
   ...baseConfig,
+
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {},
+    ignores: ['!**/*', 'node_modules', 'dist'],
   },
+
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.*?.json'],
+        tsconfigRootDir: __dirname,
+      },
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+        ...globals.browser,
+      },
+    },
+  },
+  // {
+  //   files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+  //   rules: {},
+  //   languageSettings: { parserOptions: { project: ['tsconfig.*?.json'] } },
+  // },
   {
     files: ['**/*.ts', '**/*.tsx'],
     rules: {},
@@ -14,17 +45,32 @@ module.exports = [
     files: ['**/*.js', '**/*.jsx'],
     rules: {},
   },
-  ...compat.config({ parser: 'jsonc-eslint-parser' }).map((config) => ({
-    ...config,
-    files: ['**/*.json'],
-    files: ['**/*.json'],
+
+  nodePlugin.configs['flat/recommended'],
+
+  pluginSecurity.configs.recommended,
+
+  {
     rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: ['{projectRoot}/rollup.config.{js,ts,mjs,mts}'],
-        },
-      ],
+      'n/no-missing-import': 'off',
+      'n/no-extraneous-import': 'off',
+      'n/no-extraneous-require': 'off',
+      'n/no-unpublished-require': 'off',
+      'unicorn/prefer-module': 'off',
+      'unicorn/prefer-top-level-await': 'off',
     },
-  })),
-];
+  }
+
+  // ...compat.config({ parser: 'jsonc-eslint-parser' }).map((config) => ({
+  //   ...config,
+  //   files: ['**/*.json'],
+  //   rules: {
+  //     '@nx/dependency-checks': [
+  //       'error',
+  //       {
+  //         ignoredFiles: ['{projectRoot}/vite.config.{js,ts,mjs,mts}'],
+  //       },
+  //     ],
+  //   },
+  // }))
+);
